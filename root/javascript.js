9b01c96ae6882d0086283ac4aafe7edd70e2abf2
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2018-12-08 10:57:16 -0500

    Trying to get readability to look right...

--- root/javascript.js
+++ root/javascript.js
@@ -35,15 +35,15 @@ setTimeout(() => {
     var c = document.getElementsByClassName("code")[i].innerHTML.split("");
     for (var j = 0; j < c.length - 3; j++) {
       if (c[j] === "&" && c[j + 1] === "l" && c[j + 2] === "t" && c[j + 3] === ";" && c[j + 4] === "!") {
-        c.splice(j, 0, "<div class=\"code-comment\" style=\"display: inline;\">");
+        c.splice(j, 0, "<pre class=\"code-comment\">");
         j += 6;
       }
       if (j < c.length - 3  && c[j] === "&" && c[j + 1] === "l" && c[j + 2] === "t" && c[j + 3] === ";") {
-        c.splice(j, 0, "<div class=\"code-tag\" style=\"display: inline;\">");
+        c.splice(j, 0, "<pre class=\"code-tag\">");
         j += 5;
       }
       if (j < c.length - 3 && c[j] === "&" && c[j + 1] === "g" && c[j + 2] === "t" && c[j + 3] === ";") {
-        c.splice(j + 4, 0, "</div>");
+        c.splice(j + 4, 0, "</pre>");
         j += 5;
       }
     }
