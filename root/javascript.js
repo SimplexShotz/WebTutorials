5de18dfdc9b9a20b059243b3a5ca9070e288f536
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2018-12-07 22:52:08 -0500

    test7.1

--- root/javascript.js
+++ root/javascript.js
@@ -31,7 +31,7 @@ setTimeout(() => {
   for (var i = 0; i < document.getElementsByClassName("code").length; i++) {
     var c = document.getElementsByClassName("code")[i].innerHTML.split("");
     for (var j = 0; j < c.length - 3; j++) {
-      if (c[j] === "&" && c[j + 1] === "l" && c[j + 2] === "t" && c[j + 3] === ";" && c[j + 3] === "!") {
+      if (c[j] === "&" && c[j + 1] === "l" && c[j + 2] === "t" && c[j + 3] === ";" && c[j + 4] === "!") {
         c.splice(j, 0, "<div class=\"code-comment\">");
         j += 6;
       }
